560c4b6f2e2425f69e478fad010c983d4a55a396
James McMichael <tur08816@temple.edu>
2025-11-06 14:55:33 -0500

    intent now sends message to messaging app and puts it in the body of the text

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,7 +16,17 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            if (editText.text.isNotEmpty()){
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent,null)
+                startActivity(
+                    shareIntent
+                )
+            }
         }
     }
 }
